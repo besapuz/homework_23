020c169c5af8aaddf6f3f265f828160ea525f794
besapuz <aleksandzarinskij18772@gmail.com>
2022-08-14 23:02:37 +0300

    сделал

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, request
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 
@@ -8,17 +9,40 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-@app.route("/perform_query")
+def build_query(data, cmd, value):
+    res = map(lambda v: v.strip(), data)
+    if cmd == "filter":
+        res = filter(lambda v: value in v, res)
+    if cmd == "sort":
+        revers = value == 'desc'
+        res = sorted(res, reverse=revers)
+    if cmd == "unique":
+        res = set(res)
+    if cmd == "limit":
+        value = int(value)
+        res = list(res)[:value]
+    if cmd == "map":
+        value = int(value)
+        res = map(lambda v: v.split(' ')[value], res)
+    return list(res)
+
+
+@app.route("/perform_query", methods=["POST"])
 def perform_query():
     try:
-        query = request.args
-        file_name = request.args
-    except Exception as e:
-        return e, 400
+        data = request.json
+    except KeyError:
+        return BadRequest, 400
 
+    file_name = data["filename"]
+    path_file = os.path.join(DATA_DIR, file_name)
+    if not os.path.exists(path_file):
+        raise BadRequest
 
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+    with open(path_file) as f:
+        res = build_query(f, data["cmd1"], data["value1"])
+        if "cmd2" in data and "value2" in data:
+            res = build_query(res, data["cmd2"], data["value2"])
+        res = '\n'.join(res)
+
+    return app.response_class(res, content_type="text/plain")
